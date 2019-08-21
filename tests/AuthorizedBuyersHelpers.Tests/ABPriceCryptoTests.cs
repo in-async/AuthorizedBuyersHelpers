13d03997dab23521cfb4c2a99b93144c697c3cbd
inasync <in-async@users.noreply.github.com>
2019-08-21 15:37:23 +0900

    追加 API のテストを作成。

--- tests/AuthorizedBuyersHelpers.Tests/ABPriceCryptoTests.cs
+++ tests/AuthorizedBuyersHelpers.Tests/ABPriceCryptoTests.cs
@@ -66,7 +66,34 @@ namespace AuthorizedBuyersHelpers.Tests {
 
         [TestMethod]
         public void TryEncryptPrice_ReadOnlySpan() {
-            Assert.Inconclusive();
+            Action TestCase(int testNumber, ABCrypto crypto, decimal price, byte[] inputIV, byte[] cipherBytes, (bool success, int bytesWritten, byte[] cipherBytes) expected, Type expectedExceptionType = null) => () => {
+                new TestCaseRunner($"No.{testNumber}")
+                    .Run(() => (success: ABPriceCrypto.TryEncryptPrice(crypto, price, inputIV, cipherBytes, out var bytesWritten), cipherBytes, bytesWritten))
+                    .Verify((actual, desc) => {
+                        actual.success.Is(expected.success, desc);
+                        actual.bytesWritten.Is(expected.bytesWritten, desc);
+                        actual.cipherBytes.Is(expected.cipherBytes, desc);
+                    }, expectedExceptionType);
+            };
+
+            var iv = Base16.Decode("386E3AC0000C0A080123456789ABCDEF");
+            var shortIV = Base16.Decode("386E3AC0000C0A080123456789ABCD");
+            var longIV = Base16.Decode("386E3AC0000C0A080123456789ABCDEF01");
+            new[] {
+                TestCase( 0, null   , 0m     , iv     , new byte[28], default, typeof(ArgumentNullException)),
+                TestCase( 1, _crypto, 0m     , iv     , new byte[28], (true, 28, Base64Url.Decode("OG46wAAMCggBI0VniavN7-mNy0VTOrlB8A3F8A=="))),
+                TestCase( 2, _crypto, -1.2m  , iv     , new byte[28], (true, 28, Base64Url.Decode("OG46wAAMCggBI0VniavN7xZyNLqs1wnBKd_m9w=="))),
+                TestCase( 3, _crypto, 0.123m , iv     , new byte[28], (true, 28, Base64Url.Decode("OG46wAAMCggBI0VniavN7-mNy0VTO1k5XIpbSg=="))),
+                TestCase( 4, _crypto, 1.2m   , iv     , new byte[28], (true, 28, Base64Url.Decode("OG46wAAMCggBI0VniavN7-mNy0VTKPbB3o5CMQ=="))),
+                TestCase( 5, _crypto, 123.45m, iv     , new byte[28], (true, 28, Base64Url.Decode("OG46wAAMCggBI0VniavN7-mNy0VUYQvRuIJiRw=="))),
+                TestCase( 6, _crypto, 123.45m, shortIV, new byte[28], (true, 28, Base64Url.Decode("OG46wAAMCggBI0VniavNAF7WOkwZcM1moYKxlA=="))),
+                TestCase( 7, _crypto, 123.45m, longIV , new byte[28], (true, 28, Base64Url.Decode("OG46wAAMCggBI0VniavN7-mNy0VUYQvRuIJiRw=="))),
+                TestCase( 8, _crypto, 1.2m   , iv     , new byte[27], (false, 0, new byte[27])),
+                TestCase(10, _crypto, (decimal)long.MinValue / 1_000_000            , iv, new byte[28], (true, 28, Base64Url.Decode("OG46wAAMCggBI0VniavN72mNy0VTOrlBMgsdYg=="))),
+                TestCase(11, _crypto, (decimal)long.MinValue / 1_000_000 - 0.000001m, iv, new byte[28], default, typeof(OverflowException)),
+                TestCase(12, _crypto, (decimal)long.MaxValue / 1_000_000            , iv, new byte[28], (true, 28, Base64Url.Decode("OG46wAAMCggBI0VniavN75ZyNLqsxUa-p2RaNA=="))),
+                TestCase(13, _crypto, (decimal)long.MaxValue / 1_000_000 + 1        , iv, new byte[28], default, typeof(OverflowException)),
+            }.Run();
         }
 
         [TestMethod]
@@ -92,7 +119,22 @@ namespace AuthorizedBuyersHelpers.Tests {
 
         [TestMethod]
         public void TryDecryptPrice_Span() {
-            Assert.Inconclusive();
+            Action TestCase(int testNumber, ABCrypto crypto, byte[] cipherBytes, (bool, decimal) expected, Type expectedExceptionType = null) => () => {
+                new TestCaseRunner($"No.{testNumber}")
+                    .Run(() => (ABPriceCrypto.TryDecryptPrice(crypto, cipherBytes, out var price), price))
+                    .Verify(expected, expectedExceptionType);
+            };
+
+            new[] {
+                TestCase( 0, null   , default                                                     , default, typeof(ArgumentNullException)),
+                TestCase( 1, _crypto, null                                                        , (false, 0)),
+                TestCase( 2, _crypto, new byte[0]                                                 , (false, 0)),
+                TestCase( 3, _crypto, new byte[28]                                                , (false, 0)),
+                TestCase( 5, _crypto, Base64Url.Decode("OG46wAAMCggBI0VniavN7-mNy0VTKPbB3o5CMQ=="), (true , 1.2m)),
+                TestCase( 6, _crypto, Base64Url.Decode("5nmwvgAM0UABI0VniavN72_sy3T6V9ohlpvOpA=="), (true , 1.2m)),
+                TestCase( 7, _crypto, Base64Url.Decode("5nmwvgAM0UABI0VniavN72_sy3T6V9oglpvOpA=="), (false, 0)),  // ペイロード改竄。
+                TestCase( 8, _crypto, Base64Url.Decode("OG46wAAMCggBI0VniavN7-mNy0VUYQvRuIJiRw=="), (true , 123.45m)),
+            }.Run();
         }
 
         #region Helpers
