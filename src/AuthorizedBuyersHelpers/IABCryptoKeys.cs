62a29e698ea0d3e601a44f683f19cb14c2c8935d
inasync <in-async@users.noreply.github.com>
2019-08-22 14:38:12 +0900

    IABCryptoKeys の参照実装クラス ABCryptoKeys を追加。

--- src/AuthorizedBuyersHelpers/IABCryptoKeys.cs
+++ src/AuthorizedBuyersHelpers/IABCryptoKeys.cs
@@ -1,4 +1,6 @@
-﻿namespace AuthorizedBuyersHelpers {
+﻿using System;
+
+namespace AuthorizedBuyersHelpers {
 
     /// <summary>
     /// Authorized Buyers の暗号化スキームに必要なキーセット。
@@ -18,5 +20,36 @@
         /// 整合性キー。常に非 <c>null</c>。
         /// </summary>
         byte[] IntegrityKey { get; }
+    }
+
+    /// <summary>
+    /// <see cref="IABCryptoKeys"/> の実装クラス。
+    /// </summary>
+    public class ABCryptoKeys : IABCryptoKeys {
+
+        /// <summary>
+        /// <see cref="ABCryptoKeys"/> クラスの新しいインスタンスを初期化します。
+        /// </summary>
+        /// <param name="encryptionKey"><see cref="EncryptionKey"/> に渡される値。</param>
+        /// <param name="integrityKey"><see cref="IntegrityKey"/> に渡される値。</param>
+        /// <exception cref="ArgumentNullException"><paramref name="encryptionKey"/> or <paramref name="integrityKey"/> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentException"><paramref name="encryptionKey"/> or <paramref name="integrityKey"/> is empty.</exception>
+        public ABCryptoKeys(byte[] encryptionKey, byte[] integrityKey) {
+            EncryptionKey = encryptionKey ?? throw new ArgumentNullException(nameof(encryptionKey));
+            if (encryptionKey.Length == 0) { throw new ArgumentException($"{nameof(EncryptionKey)} is empty.", nameof(encryptionKey)); }
+
+            IntegrityKey = integrityKey ?? throw new ArgumentNullException(nameof(integrityKey));
+            if (integrityKey.Length == 0) { throw new ArgumentException($"{nameof(IntegrityKey)} is empty.", nameof(integrityKey)); }
+        }
+
+        /// <summary>
+        /// <see cref="IABCryptoKeys.EncryptionKey"/> の実装。
+        /// </summary>
+        public byte[] EncryptionKey { get; }
+
+        /// <summary>
+        /// <see cref="IABCryptoKeys.IntegrityKey"/> の実装。
+        /// </summary>
+        public byte[] IntegrityKey { get; }
     }
 }
---
+++ tests/AuthorizedBuyersHelpers.Tests/ABCryptoKeysTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Inasync;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AuthorizedBuyersHelpers.Tests {
+
+    [TestClass]
+    public class ABCryptoKeysTests {
+
+        [TestMethod]
+        public void Ctor() {
+            Action TestCase(int testNumber, byte[] eKey, byte[] iKey, Type expectedExceptionType = null) => () => {
+                new TestCaseRunner($"No.{testNumber}")
+                    .Run(() => new ABCryptoKeys(eKey, iKey))
+                    .Verify((actual, desc) => {
+                        actual.EncryptionKey.Is(eKey, desc);
+                        actual.IntegrityKey.Is(iKey, desc);
+                    }, expectedExceptionType);
+            };
+
+            new[] {
+                TestCase( 0, null                    , Rand.Bytes(minLength: 1), typeof(ArgumentNullException)),
+                TestCase( 1, new byte[0]             , Rand.Bytes(minLength: 1), typeof(ArgumentException)),
+                TestCase( 2, Rand.Bytes(minLength: 1), null                    , typeof(ArgumentNullException)),
+                TestCase( 3, Rand.Bytes(minLength: 1), new byte[0]             , typeof(ArgumentException)),
+                TestCase(10, Rand.Bytes(minLength: 1), Rand.Bytes(minLength: 1)),
+            }.Run();
+        }
+    }
+}
